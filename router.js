87069a1f08a06f6c0810623089f605910477fa36
Bignoes <66350685+Bignoes@users.noreply.github.com>
2020-08-21 18:58:07 +0800

    Update router.js

--- router.js
+++ router.js
@@ -45,38 +45,26 @@ router.get('/user', function(req, res) {
 })
 
 // 处理注册数据
-router.post('/user', async function(req, res) {
+router.post('/user', function(req, res) {
     var body = req.body
-    try {
-        if (await User.findOne({ email: body.email })) {
-            return res.status(200).json({
-                err_code: 1,
-                message: '邮箱已存在'
-            })
-        }
-        if (await User.findOne({ nickname: body.nickname })) {
-            return res.status(200).json({
-                err_code: 2,
-                message: '昵称已存在'
-            })
-        }
-        // 对密码进行 md5 重复加密
-        body.password = md5(md5(body.password))
 
-        // 创建用户，执行注册
-        await new User(body).save()
-        req.session.user = new User(body)
+    // 对密码进行 md5 重复加密
+    body.password = md5(md5(body.password))
+
+    // 创建用户，执行注册
+    new User(body).save(function(error, user) {
+        if (error) {
+            return res.status(500).json({
+                err_code: 500
+            })
+        }
         res.status(200).json({
             err_code: 0,
-            user: body
+            user
         })
-    } catch (err) {
-        return res.status(500).json({
-            err_code: 500,
-            message: err.message
-        })
-    }
+    })
 })
+
 
 // 修改资料
 router.put('/user', function(req, res) {
